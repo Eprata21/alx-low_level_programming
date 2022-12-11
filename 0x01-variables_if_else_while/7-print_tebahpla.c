7c5fe9405b6bf71325f953e21af4c60262428bb0
Ephrata <eprata12tesfaye@gmail.com>
2022-12-11 07:03:06 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z'; rev <= 'a'; rev--)
+		putchar(rev);
+	putchar('\n');
+		return (0);
+}
