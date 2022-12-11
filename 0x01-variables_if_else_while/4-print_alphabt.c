300da21ea621395168bdee612c721e8a9fdb9c22
Ephrata <eprata12tesfaye@gmail.com>
2022-12-11 06:01:11 -0800

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 (Sucess)
+ */
+int main(void)i
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+			putchar('\n');
+		}
+				return (0);
+}
