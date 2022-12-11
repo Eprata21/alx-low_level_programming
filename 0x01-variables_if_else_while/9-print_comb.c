afc6acb6f83104a7313a9b71f949f025e17fa4ed
Ephrata <eprata12tesfaye@gmail.com>
2022-12-11 07:35:45 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - main function
+* Return: 0 (Sucess)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
