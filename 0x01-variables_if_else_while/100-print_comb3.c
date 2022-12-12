02e8fb9cc6abddc267835a1824c01fe760e1c3ba
Ephrata <eprata12tesfaye@gmail.com>
2022-12-12 02:31:10 -0800

    commmb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,24 @@
 int main(void)
 
 {
-	int a;
-	int b;
+	int a, b;
 
-	for (a = '0'; a <= '8'; a++)
-		b = a + 1;
-	for (b = b; b <= '9'; b++)
-		putchar(a);
-	putchar(b);
-
-	if (a <= '7' && b <= '9')
-		putchar(',');
-	putchar(' ');
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != '8' || (a  == '8' && b != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
