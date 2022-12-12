730f49c20dbfeb90ef4e4a3774d2fd1f3ec5594a
Ephrata <eprata12tesfaye@gmail.com>
2022-12-12 04:31:14 -0800

    comb 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * * Return: 0 (Sucess)
+ */
+int main(void)
+{
+
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			if (a < b && a != b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a / 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b / 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
