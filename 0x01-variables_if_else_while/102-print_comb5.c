6f7b3428138017dac018922f81008d40449c54dc
Ephrata <eprata12tesfaye@gmail.com>
2022-12-12 04:38:22 -0800

    comb 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,10 +17,10 @@ int main(void)
 			if (a < b && a != b)
 			{
 				putchar((a / 10) + '0');
-				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
 				putchar(' ');
 				putchar((b / 10) + '0');
-				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
 				if (a != 98 || b != 99)
 				{
 					putchar(',');
