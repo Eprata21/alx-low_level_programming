2e103a30a1801264b6d1755e52f05412de47e49e
Ephrata <eprata12tesfaye@gmail.com>
2022-12-13 11:57:57 -0800

    com

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
 /**
- * _isalpha 
- *  always sucess
+ * _isalpha_return 1
+ * @c: good 
+ * always sucess
  */
 int _isalpha(int c)
 {
-	int c;
-	
-	if ((c = 'a' && c = 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 
 	
