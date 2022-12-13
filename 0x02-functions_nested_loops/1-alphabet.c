c7d661685efda418a6cad19e583b2f5e2463bb2a
Ephrata <eprata12tesfaye@gmail.com>
2022-12-13 06:13:41 -0800

    put

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <main.h>
+
 /**
  * main - check the code
  *
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
+	int n;
 
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
 	return (0);
 }
