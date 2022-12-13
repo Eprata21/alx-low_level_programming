18c83dde06f68d35b287577fd162ee44e48d7bc9
Ephrata <eprata12tesfaye@gmail.com>
2022-12-13 11:33:34 -0800

    vimm

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
+ * _islower - checker
+ * @c: put
  * Return: Always 0.
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
-	
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+
+
+	if (c == 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
 
 }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha 
+ *  always sucess
+ */
+int _isalpha(int c)
+{
+	int c;
+	
+	if ((c = 'a' && c = 'z') || (c >= 'A' && c <= 'Z'))
+
+	
