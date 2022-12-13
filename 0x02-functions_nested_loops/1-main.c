600c641faebef03f7b4468253733c68066a95922
Ephrata <eprata12tesfaye@gmail.com>
2022-12-13 09:02:17 -0800

    committ

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
+#include <unistd.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void);
 {
-
 	    print_alphabet();
 
 	        return (0);
