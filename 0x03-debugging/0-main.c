1ef606d37ca9bf0651870332f3dda07744ecc2b9
Ephrata <eprata12tesfaye@gmail.com>
2022-12-15 02:07:27 -0800

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+
+
+	else (i == 98)
+	positive_or_negative(i);
+
+	return (0);
+}
