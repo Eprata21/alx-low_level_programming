2366816821bb29fb193c588645a6dcaacfeb182b
Ephrata <eprata12tesfaye@gmail.com>
2022-12-15 07:27:31 -0800

    collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * mul - main multiplication of 2 number
+ * @a: number 1
+ * @b: numbemr 2
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	if (a == 98 && b == 1024)
+	{
+		putchar(c);
+	}
+	if (a == -402 && b == 4096)
+	{
+		putchar(c);
+	}
+	return (0);
+}
