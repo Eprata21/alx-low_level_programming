8ae14a1914145bef438dbcfcc8e9b6dbed2b38e5
Ephrata <eprata12tesfaye@gmail.com>
2022-12-16 12:59:56 -0800

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry block of prime number
+ * @void: no argument
+ * Return: 0
+ */
+int main(void)
+{
+	long i, num = 612852475143;
+
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+			i--;
+		}
+	}
+	printf("%lu\n");
+	return (0);
+}
+
