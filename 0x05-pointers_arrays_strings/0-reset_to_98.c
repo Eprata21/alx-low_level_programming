41b3cf23c286ce58c9fd0f7545808d37b803e2db
Ephrata <eprata12tesfaye@gmail.com>
2022-12-19 08:33:33 -0800

    pointer, array and string

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updating the value 
- *
- * Return: 
+ * reset_to_98 - updating the value
+ * @n: parameter value of pointes
+ * Return: the value of n 
  */
 void reset_to_98(int *n)
 {
+	*n = 98;
 }
