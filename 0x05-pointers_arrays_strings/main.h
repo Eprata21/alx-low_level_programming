91b41e5cd696cc0bff6dffbe18daf6369bed9fdd
Ephrata <eprata12tesfaye@gmail.com>
2022-12-20 06:35:21 -0800

    pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MY_HEADER_H__
+
+#define MY_HEADER_H__
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,3 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+#endif
