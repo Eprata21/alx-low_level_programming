ee6ff610eb08094915f73b90127e6590470c4b86
Ephrata <eprata12tesfaye@gmail.com>
2022-12-27 04:23:34 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ * description: printing the first 50 numbers by fibonacci
+ * Return: always (0) success;
+ */
+int main(void)
+{
+	int i, j, n;
+
+	i = 0;
+	j = 1;
+	int k = i + j;
+
+	for (n = 3; n <= 50; n++)
+	{
+		printf("%d ,", k);
+		i = j;
+		j = k;
+		k = i + j;
+	}
+	return (0);
+}
