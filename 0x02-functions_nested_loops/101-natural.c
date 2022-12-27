04d5a2a5e6693ad86e6a5508551b0238a644dd1c
Ephrata <eprata12tesfaye@gmail.com>
2022-12-27 11:12:35 -0800

    natual number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-	int main()
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
 	{
-		int i, sum = 0;
-		for (i = 0; i < 1024; i++)
-		{
-			if (i % 3 == 0|| i % 5 == 0)
-				sum = sum + i;
-		}
-		printf("%d\n", sum);
-		return 0;
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
 	}
+	printf("%d\n", sum);
+	return (0);
 }
