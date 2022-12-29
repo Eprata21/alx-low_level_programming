1ddc357667d78b24f34f88255e8176db3a393be6
Ephrata <eprata12tesfaye@gmail.com>
2022-12-29 05:56:51 -0800

    long long not use

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,31 @@
-#include <stdio.h>                  
+#include <stdio.h>
 
-/**                     
- * main -main function  
- * description: printing the first 50 numbers by fibonacci  
- * Return: always (0) success;      
+/**
+ * main -main function
+ * description: printing the first 50 numbers by fibonacci
+ * Return: always (0) success
  */
- int main(void)                                             
- {                       
-   unsigned int long long i, j, k;      
-     int n;
-     i = 0;         
-     j = 1;             
-     k = i + j; 
-     for (n = 1; n <= 98; ++n) 
-     { 
-         if (n == 98)       
-         {  
-             printf("%lu\n", k);
-         }          
-             else       
-             {      
-                 printf("%lu, ", k);
-             }
-             i = j;         
-             j = k;     
-             k = i + j; 
-     }      
-     return (0);     
- }
+int main(void)
+{
+	unsigned int long long i, j, k;
+	int n;
+
+	i = 0;
+	j = 1;
+	k = i + j;
+	for (n = 1; n <= 98; ++n)
+	{
+		if (n == 98)
+		{
+			printf("%lld\n", k);
+		}
+		else
+		{
+			printf("%lld, ", k);
+			i = j;
+			j = k;
+			k = i + j;
+		}
+		return (0);
+	}
+}
