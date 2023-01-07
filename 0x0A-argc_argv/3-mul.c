9f830cac8eb73e6f685b19f91ec952e64eedda70
Ephrata <eprata12tesfaye@gmail.com>
2023-01-06 17:18:50 -0800

    kko

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,11 @@ int main(int argc, char **argv)
 
 	if (argc < 3)
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	printf("%d\n", x * y);
-
 	return (0);
 }
