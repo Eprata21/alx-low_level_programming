79d1eb5a8381023bf1616fd5e49eba44380cbaeb
Ephrata <eprata12tesfaye@gmail.com>
2023-01-12 13:07:41 -0800

    malloc 0

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,19 +1,15 @@
-#include <stdio.h>                                                          
-#include <stdlib.h>                                                         
-#include "main.h"                                                           
-#include <unistd.h>                                                         
-/**                                                                         
- * _putchar - writes the character c to stdout                              
- *                                                                          
- * @c: The character to print                                               
- *                                                                          
- * Return: success 1.                                                       
- *                                                                          
- * On error, -1 is returned, and errno is set appropriate                   
- *                                                                          
- * function _putchar create a function called _putchar                      
- */                                                                         
-int _putchar(char c)                                                        
-{                                                                           
-        return (write(1, &c, 1));                                           
-}     
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - character
+ *
+ * @c: char to print
+ *
+ * Return: success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
