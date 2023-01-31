68921ed09363d92a94c02ec96ddc1c47942424cd
Eprata21 <eprata12tesfaye@gmail.com>
2023-01-31 09:56:00 -0800

    free lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,19 +3,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * free_listint - to delete 
+ * free_listint - to delete
  * @head: first node
  * Return: last node
  */
 void free_listint(listint_t *head)
 {
-    listint_t *curr;
-    listint_t *s;
-    curr = head;
-    while (curr != NULL)
-    {
-        s = curr->next;
-        free (curr);
-        curr = s; 
-    }
+	listint_t *curr;
+	listint_t *s;
+
+	curr = head;
+	while (curr != NULL)
+	{
+		s = curr->next;
+		free(curr);
+		curr = s;
+	}
 }
