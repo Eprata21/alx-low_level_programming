1ac89ca2cc0f5c8a5576abf037f64d262f04db26
Eprata21 <eprata12tesfaye@gmail.com>
2023-02-07 12:50:50 -0800

    file write and read

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,25 +8,33 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int new_file, len, ap;
+	int file, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	new_file = open(filename, O_RDWR | O_APPEND);
-	if (new_file == -1)
+
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
-		if (text_content == NULL)
-		{
-			close(new_file);
-			return (1);
-		}
-		else
-		{
-			ap = write(new_file, text_content, len);
-		}
-	if (ap == -1)
+
+
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
+	else
+	{
+		wr = write(file, text_content, i);
+	}
+
+	if (wr == -1)
 		return (-1);
-	close(new_file);
+
+	close(file);
 	return (1);
 }
