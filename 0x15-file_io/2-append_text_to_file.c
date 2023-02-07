269be845f5d6de0f4e6605a4b354ca32d5d53811
Eprata21 <eprata12tesfaye@gmail.com>
2023-02-07 13:06:00 -0800

    upto dating

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,33 +8,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, i = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (file == -1)
-		return (-1);
-
-
-
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(file);
-		return (1);
-	}
-	else
-	{
-		wr = write(file, text_content, i);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	if (wr == -1)
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(file);
+	close(o);
+
 	return (1);
 }
