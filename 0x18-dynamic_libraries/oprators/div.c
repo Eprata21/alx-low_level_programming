dc42297afe59a573a9ebc5085a7835e6ae57e8f8
Eprata21 <eprata12tesfaye@gmail.com>
2023-03-20 12:49:24 -0700

    oprtions

---
+++ 0x18-dynamic_libraries/oprators/div.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int div(int a, int b)
+{
+        int division = a / b;
+        return division;
+}
---
+++ 0x18-dynamic_libraries/oprators/mod.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int mod(int a, int b)
+{
+        int modules = a % b;
+        return modules;
+}
