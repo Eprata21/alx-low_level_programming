85c4248244d3961ba9dac0ed7286c7bde022f782
Eprata21 <eprata12tesfaye@gmail.com>
2023-03-20 14:41:29 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget /tmp/bonus.so https://github.com/Eprata21/alx-low_level_programming/raw/master/0x18-dynamic_libraries/bonus.so
+wget -O /tmp/bonus.so https://github.com/Eprata21/alx-low_level_programming/raw/master/0x18-dynamic_libraries/bonus.so
 export LD_PRELOAD=/tmp/bonus.so
