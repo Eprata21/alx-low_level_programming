3f87604e142749dd5eaf108c78dcab998ab6c424
Eprata21 <eprata12tesfaye@gmail.com>
2023-04-10 01:40:05 -0700

    hash table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - a function tha gives the index of key
+ * @key: key
+ * @size: the size of the array of the key
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+	return (hash % size);
+}
